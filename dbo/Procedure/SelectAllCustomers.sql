bfc8d995e3845f92245507b8984906d71d0ab0f5
dbm_sc_user_1 <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>
2021-05-20 11:37:22 +0000

    MTN Demo

--- dbo/Procedure/SelectAllCustomers.sql
+++ dbo/Procedure/SelectAllCustomers.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE [dbo].[SelectAllCustomers]
 AS
-SELECT * FROM Customers -- new fix
+SELECT * FROM Customers -- Demo for MTN
 GO
 
---
+++ dbo/Table/MTN.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[MTN] (
+    [MTN] NCHAR (10) NULL
+);
+
+
+GO
+
